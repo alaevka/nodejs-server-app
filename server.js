4be130d2b62bc709494083bebb8c5aadb34f32b2
kirill alaev <alaevka@gmail.com>
2015-05-19 15:06:22 +0300

    all not good

--- server.js
+++ server.js
@@ -1,42 +1,16 @@
 var app = require('express')();
-//var Chance = require('chance');
-//var chance = new Chance();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+
+var mongo = require("mongodb");
+var mongodbUri = "mongodb://127.0.0.1/laravel5"
+
 //var started = 'stopped';
 app.get('/', function(req, res){
   res.sendFile('client.html', { root: path.join(__dirname, './') });
 });
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-// 	console.log('user disconnected');
-//   });
-//   socket.on('action', function(data){
-// 	console.log('received action');
-    
-// 	if(data.todo=='start') {
-// 		console.log('action name is START');
-// 		if(started != 'started') {
-// 			t=setInterval( function() {
-// 				var random_coordinates = chance.coordinates();
-// 				console.log(random_coordinates);
-// 				io.emit('generated', random_coordinates);
-// 			}, 1000);
-// 			started = 'started';
-// 		} else {
-// 			console.log('already started');
-// 		}
-// 	} else if(data.todo=='stop') {
-// 		console.log('action name is STOP');
-//         clearInterval(t);
-// 		started = 'stopped';
-// 	}
-	
-//   });
-// });
 
 
 io.on('connection', function(socket){
@@ -45,8 +19,17 @@ io.on('connection', function(socket){
 	console.log('user disconnected');
   });
   socket.on('data_send', function(data){
-	//console.log('received data');
-    console.log(data.coordinates+', '+socket.id);
+
+    //console.log(data.coordinates+', '+socket.id);
+    mongo.MongoClient.connect(mongodbUri, function (err, db) {
+        if (err) throw err;
+        console.log("Connected to Database");
+        db.collection('coordinates').insert({coordinates: data.coordinates, time: Date.now(), uid: socket.id}, function(err, records) {
+            if(err) throw err;
+            console.log("Record added");
+        });
+    });
+
 	
   });
   socket.on('event_send', function(data){
